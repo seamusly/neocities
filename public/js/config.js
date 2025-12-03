e8d43b020c0779679b0b6d8f8a55bfc203205ff4
seamusly <59736643+seamusly@users.noreply.github.com>
2025-12-03 15:30:41 -0500

    updates for site

---
+++ public/js/config.js
@@ -0,0 +1,26 @@
+let config = {};
+
+config.footer = {
+    links: [
+        {
+            title: 'Sitemap',
+            link: 'sitemap.html',
+        },
+        {
+            title: 'Credits',
+            link: 'credits.html',
+        },
+    ]
+};
+
+config.pages = {
+    beyondthesea: {
+        title: {
+            augurys: 'Augurys',
+            zane: 'Zane'
+        },
+        subtitle: '✦ PMD Beyond the Sea ✦'
+    }
+}
+
+export { config };
