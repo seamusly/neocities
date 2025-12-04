90baf625bd40bd15e6bb677eb41887a21b2b5dd8
seamusly <59736643+seamusly@users.noreply.github.com>
2025-12-04 02:09:26 -0500

    zane profile setup

--- public/js/config.js
+++ public/js/config.js
@@ -15,12 +15,50 @@ config.footer = {
 
 config.pages = {
     beyondthesea: {
+        root: '../../',
         title: {
             augurys: 'Augurys',
             zane: 'Zane'
         },
         subtitle: '✦ PMD Beyond the Sea ✦'
     }
+}
+
+config.filters = {
+    collab: [
+        'solo',
+        'collab'
+    ],
+    medium: [
+        'artwork',
+        'writing'
+    ],
+    submission: [
+        'dungeon',
+        'task'
+    ],
+    event: [
+        'holidayhills',
+        'mission'
+    ],
+    year: [
+        '2022',
+        '2023',
+        '2024',
+        '2025',
+        '2026'
+    ],
+    season: [
+        'winter',
+        'spring',
+        'summer',
+        'fall'
+    ],
+    design: [
+        'olddesign',
+        'gijinka',
+        'feral'
+    ],
 }
 
 export { config };
---
+++ public/js/utils.js
@@ -0,0 +1,64 @@
+let utils = {};
+
+utils.init = {
+    header(title, subtitle) {
+        const header = $(`.page-header`);
+
+        if (subtitle) {
+            header.prepend(`
+                <div class="page-header-subtitle">${subtitle}</div>
+            `);
+        }
+
+        header.prepend(`
+            <h1 class="page-header-title mb-0">${title}</h1>
+        `);
+    },
+
+
+    footer(linksArr, root) {
+        const footer = $(`.footer-content`);
+        footer.append('<span>✦</span>');
+
+        linksArr.forEach((elem) => {
+            footer.append(`
+                <span class="mx-lg-3 mx-2">
+                    <a href="${root}${elem.link}">${elem.title}</a>
+                </span>
+                ${linksArr.indexOf(elem) < linksArr.length - 1 ? `<span>✦</span>` : ``}
+            `);
+        });
+
+        footer.append('<span>✦</span>');
+    },
+
+
+    top() {
+        $('body').append(`
+            <div class="back-to-top"><a href="#"><i class="fa-solid fa-arrow-up"></i></a></div>
+        `);
+    },
+
+
+    profile() {
+        $(`.profile-collapse-link`).on('click', function() {
+            $(this).toggleClass('active');
+        });
+    },
+
+
+    tooltips() {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    },
+
+
+    lightbox(resizeDuration = 400, fadeDuration = 200) {
+        lightbox.option({
+            'resizeDuration': resizeDuration,
+            'fadeDuration': fadeDuration,
+        });
+    }
+}
+
+export { utils };
